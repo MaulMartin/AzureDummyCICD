c62ebf1d19fb5f1a05858c119022445d9001750d
Serhii Bilovol <themaulmartin@gmail.com>
2021-11-01 12:49:27 +0200

    Removed unused files

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dotnet restore
-dotnet build DummyCDCI --runtime linux-x64 --configuration Release
-dotnet publish DummyCDCI --runtime linux-x64 --configuration Release -o /app/publish
--- build_local.sh
+++
@@ -1,5 +0,0 @@
-docker run \
--v `pwd`:/src \
--v `pwd`/app:/app \
--it mcr.microsoft.com/dotnet/sdk:5.0 \
-/bin/bash 
