fa0ff2d471fa0fe987716d5a07c1f6acff4fb8cd
Serhii Bilovol <themaulmartin@gmail.com>
2021-11-03 10:54:54 +0200

    Load Gen edit

---
+++ LoadGenerator/LoadGen.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Http;
+
+namespace LoadGenerator
+{
+    public static class LoadGen
+    {
+        public static int AttemptsCount = 10;
+        public static int TimeoutSpanSec = 0;
+
+        public static async void GenereateLoad()
+        {
+            int failCount = 0;
+            int succCount = 0;
+
+            try
+            {
+                for (int i = 1; i <= AttemptsCount; i++)
+                {
+                    HttpResponseMessage res;
+
+                    using (var client = new HttpClient())
+                    using (HttpResponseMessage response = await client.GetAsync("https://dummycicd.azurewebsites.net/api/test").ConfigureAwait(false))
+                    {
+                        res = response;
+                    }
+
+                    if (res.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        succCount++;
+                    }
+                    else
+                    {
+                        failCount++;
+                    }
+
+                    Console.WriteLine($"Reques #{i} status: {res.StatusCode}");
+
+                    if(TimeoutSpanSec > 0)
+                    {
+                        System.Threading.Thread.Sleep(TimeoutSpanSec * 1000);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine($"Succesful requests count: {succCount}");
+            Console.WriteLine($"Failed requests count: {failCount}");
+        }
+    }
+}
+
+
---
+++ LoadGenerator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LoadGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoadGen.AttemptsCount = 500;
+            LoadGen.TimeoutSpanSec = 3;
+
+            LoadGen.GenereateLoad();
+            Console.ReadKey();
+        }
+    }
+}
